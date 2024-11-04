df6559d39d0d73ad153b20bf4d8624d4fb739dab
Daniel <u22099dandev@gmail.com>
2024-11-04 21:30:13 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useCamera } from "./custom-hooks/useCamera";
 function App() {
   const { image, stream, error, captureImage,requestCamera } = useCamera();
+  const vid = useRef();
   
   useEffect(() => {
     requestCamera();
-  })
+  });
+  useEffect(() => {
+    if(stream && vid.current){
+      vid.current.srcObject = stream;
+    }
+  }, [stream, vid.current]);
+  useEffect(() => {
+    if(error){
+      console.log(error);
+    }
+  }, [error]);
   return (
     <>
-      <p>Hello World</p>
-      <video muted autoplay src={stream} />
+      <video ref={vid} muted autoPlay/>
+      <button onClick={() => captureImage()}> Capture Image</button>
+      {image && <img src={image}/>}
+      {error && <p>An error occured</p>}
     </>
   )
 }
