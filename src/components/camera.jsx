1068d27e4975bd70e7293795dbd7a8e41de4d971
U22099 <nifemiolaniyi4@gmail.com>
2024-11-05 21:37:32 +0100

    update camera.jsx

--- src/components/camera.jsx
+++ src/components/camera.jsx
@@ -26,9 +26,9 @@ function Camera() {
   }, [stream, vid.current]);
   return (
     <>
-    <div className="p-5 w-[90vw] h-full flex flex-col gap-3 mx-auto mt-10">
+    <div className="p-5 w-[90vw] h-full flex flex-col gap-3 mx-auto mt-10 bg-black">
       <video ref={vid} playsInline webkit-playsinline className="object-cover"/>
-      <button className="bg-purple-600 shadow-lg text-[gold] p-4 rounded-lg font-bold shadow-purple-300 active:shadow-none" onClick={() => captureImage()}> Capture Image</button>
+      <button className="bg-cyan-600 shadow-lg text-[gold] p-4 rounded-lg font-bold shadow-emerald-400  active:shadow-none" onClick={() => captureImage()}> Capture Image</button>
       {image && <img src={image}/>}
       {error && <p className="text-red-600">An error occured</p>}
     </div>
@@ -40,7 +40,7 @@ function Camera() {
 const InitButton = ({ start }) => {
   return(
    <div className="fixed h-full w-full top-0 flex justify-center items-center bg-black">
-     <button className="bg-purple-600 shadow-lg text-[gold] p-4 rounded-lg font-bold shadow-purple-300 active:shadow-none" onClick={async () => await start()}>Start Camera</button>
+     <button className="bg-cyan-600 shadow-lg text-[gold] p-4 rounded-lg font-bold shadow-emerald-500 active:shadow-none" onClick={async () => await start()}>Start Camera</button>
    </div> 
   )
 }
