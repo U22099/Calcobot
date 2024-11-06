7ad6c150f6b09111cbeb126abb7c58ac00b53bb0
Daniel <u22099dandev@gmail.com>
2024-11-06 01:12:44 +0000

    update

--- src/components/result-components/image-submit-page.jsx
+++ src/components/result-components/image-submit-page.jsx
@@ -1,7 +1,7 @@
 import { useData } from "../../store";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { FaSpinner } from "react-icons/fa6";
+import { ImSpinner3 } from "react-icons/im";
 import { getMathsSolution } from "../../utils/getMathsSolution";
 
 export default function Image({ setShowSolution, setSolution }){
@@ -10,7 +10,7 @@ export default function Image({ setShowSolution, setSolution }){
   const [ loading, setLoading ] = useState();
   const fetch = async () => {
     setLoading(true);
-    const solution = await getMathsSolution();
+    const solution = await getMathsSolution(data);
     setSolution(solution);
     setShowSolution(true);
   }
@@ -21,7 +21,7 @@ export default function Image({ setShowSolution, setSolution }){
       <div className="flex gap-2 justify-between items-center w-full">
         <button className="flex justify-center items-center text-center bg-white text-black fill-black font-bold py-2 px-4 rounded shadow-sm active:shadow-none active:scale-95 mx-auto text-lg w-full" onClick={() => navigate("/")}>Retake</button>
         <button className="flex justify-center items-center text-center bg-white text-black fill-black font-bold py-2 px-4 rounded shadow-sm active:shadow-none active:scale-95 mx-auto text-lg w-full" onClick={async () => await fetch()}>
-        {loading ? <FaSpinner className="animate-spin"/> : "Submit"}
+        {loading ? <ImSpinner3 className="animate-spin scale-105"/> : "Submit"}
         </button>
       </div>
     </div>
--- src/components/result-components/solution.jsx
+++ src/components/result-components/solution.jsx
@@ -1,20 +1,74 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import markdownToHtml from "../../utils/markdown-to-html";
+import markdownToText from "../../utils/markdown-to-txt";
+import copyToClipboard from "../../utils/copy-to-clipboard";
+import textToSpeech from "../../utils/text-to-speech";
+import textToFile from "../../utils/text-to-file";
+import getMathsSolution from "../../utils/getMathsSolution";
+import { useData } from "../../store";
+
+import { IoCaretBackSharp } from "react-icons/io5";
+import { RiRefreshLine } from "react-icons/ri";
+import { IoCopyOutline } from "react-icons/io5";
+import { HiMiniSpeakerWave } from "react-icons/hi2";
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
+import { FaStop } from "react-icons/fa";
+import { IoDownloadOutline } from "react-icons/io5";
 
 export default function Solution({ solution }){
-  const convertMarkdown = () => {
-    return markdownToHtml(solution);
+  const [speaking, setSpeaking] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const data = useData(state => state.data);
+  const navigate = useNavigation();
+  const [htmlText, setHtmlText] = markdownToHtml("**hello**");
+  const text = () => markdownToText(solution);
+  const copy = () => {
+    copyToClipboard(text());
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+  const speak = () => {
+    if(speaking){
+      textToSpeech();
+      setSpeaking(false);
+      return;
+    };
+    textToSpeech(text()); 
+    setSpeaking(true);
+  }
+  const download = () => textToFile(text(), "Maths-Solution")
+  const refresh = async () => {
+    setLoading(true)
+    const markdown = await getMathsSolution(data);
+    setHtmlText(markdownToText(markdown));
+    setLoading(false);
   }
   return (
-    <div className="bg-black py-10 px-2 gap-2 flex flex-col">
+    <div className="bg-black py-10 px-2 gap-2 flex flex-col min-h-[100vh]">
       <header>
-        <h1 className="text-2xl font-bold">Calcobot</h1>
+        <h1 className="text-2xl font-bold text-white">Calcobot</h1>
       </header>
-      <section>
-      XXX
+      <section className="flex gap-2">
+        <div className="flex justify-center items-center text-center bg-white fill-black text-2xl p-2 rounded shadow-sm active:shadow-none active:scale-95" onClick={() => navigate("/")}>
+          <IoCaretBackSharp />
+        </div>
+        <div className="flex justify-center items-center text-center bg-white fill-black text-2xl p-2 rounded shadow-sm active:shadow-none active:scale-95" onClick={() => refresh()}>
+          <RiRefreshLine className={loading&&"animate-spin"}/>
+        </div>
+        <div className="flex justify-center items-center text-center bg-white fill-black text-2xl p-2 rounded shadow-sm active:shadow-none active:scale-95" onClick={() => copy()}>
+          {copied ? <IoCheckmarkDoneOutline /> : <IoCopyOutline />}
+        </div>
+        <div className="flex justify-center items-center text-center bg-white fill-black text-2xl p-2 rounded shadow-sm active:shadow-none active:scale-95" onClick={() => speak()}>
+          {speaking ? <FaStop /> : <HiMiniSpeakerWave />}
+        </div>
+        <div className="flex justify-center items-center text-center bg-white fill-black text-2xl p-2 rounded shadow-sm active:shadow-none active:scale-95" onClick={() => download()}>
+          <IoDownloadOutline />
+        </div>
       </section>
-      <section className="bg-neutral-700 p-2 rounded-md text-lg h-fit w-full" dangerouslySetInnerHTML={{
-        __html: convertMarkdown()
+        <section className="bg-white p-2 rounded-md text-lg h-fit w-full min-h-20 display" dangerouslySetInnerHTML={{
+        __html: htmlText
       }}>
       </section>
     </div>
